ab5c3344d6758755ec982113a8927b8e3515142c
michelle <31318@ma-web.nl>
2021-11-05 16:29:11 +0100

    made invetory Abstract

---
+++ Inventory_Abstract/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Inventory_Abstract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // check if everything works
+            Weapon saber = new Weapon();
+            Armor kingsCrown = new Armor();
+            saber.Equip();
+            kingsCrown.Equip();
+        }
+    }
+
+    // Abstract base class for Armor and Weapon
+    abstract class Equiment
+    {
+        public virtual void Equip()
+        {
+            Console.WriteLine("Equiped");
+        }
+    }
+
+    // inherited class
+    class Armor : Equiment
+    {
+        // overrides the base class its function
+        public override void Equip()
+        {
+            Console.WriteLine("Equiped");
+            Console.WriteLine("Boost stats and update on character visuals");
+            Console.WriteLine("consumed armor slot");
+        }
+    }
+
+    class Weapon : Equiment
+    {
+        // overrides the base class its function
+        public override void Equip()
+        {
+            Console.WriteLine("Equiped");
+            Console.WriteLine("Boost stats and update on character visuals");
+            Console.WriteLine("consumed weapon slot");
+        }
+    }
+}
